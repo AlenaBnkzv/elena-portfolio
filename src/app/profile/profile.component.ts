768dc4a74ba8e22bd5e7e74dbe38a5e337183557
AlenaBnkzv <elena.bnkzv@gmail.com>
2019-06-25 22:51:34 +0300

    profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,25 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProfileService }  from './profile.service';
+import { Subscription }  from 'rxjs';
+
+interface User {
+  name: string;
+  git: string;
+  gitUrl: string;
+  photoUrl: string;
+}
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent implements OnInit {
-  userName: string;
-  userGit: string;
-  userGitUrl: string;
-  userPhoto: string;
+export class ProfileComponent implements OnInit, OnDestroy {
+  user: User;
+  userSub: Subscription;
 
   constructor(private profile: ProfileService) { }
 
   ngOnInit() {
-    this.profile.getUser().subscribe((data)=> {
-      this.userName = data.name;
-      this.userGit = data.git;
-      this.userGitUrl = data.gitUrl;
-      this.userPhoto = data.photoUrl;
+    this.userSub = this.profile.getUser().subscribe((data)=> {
+      this.user = data;
     });
   }
+  ngOnDestroy() {
+    if (this.userSub) {
+      this.userSub.unsubscribe();
+    }
+  }
+
 }

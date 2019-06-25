aeb1c2c12fc64cc2e1f0d2591e932c7fe0d2c07d
AlenaBnkzv <elena.bnkzv@gmail.com>
2019-06-25 17:39:20 +0300

    added firebase modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
+const config = {
+  apiKey: "AIzaSyCfM3vQmnsCeqfj--tyghF9nUbSna4-u0A",
+    authDomain: "portfolio-data-a53b6.firebaseapp.com",
+    databaseURL: "https://portfolio-data-a53b6.firebaseio.com",
+    projectId: "portfolio-data-a53b6",
+    storageBucket: "",
+    messagingSenderId: "521113394113",
+    appId: "1:521113394113:web:b5d39f15b1a02732"
+  };
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(config),
+  ],
   declarations: [ AppComponent, HomeComponent ],
   bootstrap:    [ AppComponent ]
 })

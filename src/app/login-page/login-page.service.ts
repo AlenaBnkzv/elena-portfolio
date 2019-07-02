9d11052c58f3b3804cfcb6eba9bcb6de5164dbd4
AlenaBnkzv <elena.bnkzv@gmail.com>
2019-07-03 00:23:07 +0300

    2 files modified

--- src/app/login-page/login-page.service.ts
+++ src/app/login-page/login-page.service.ts
@@ -1,22 +1,21 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { AngularFireAuthModule, AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireAuthModule, AngularFireAuth } from '@angular/fire/auth';
 import * as firebase from '@firebase/app';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginPageService {
-  user: Observable<firebase.User>;
 
   constructor(private fireAuth: AngularFireAuth) {
   }
 
-    login() {
-
-    this.fireAuth.auth.signInWithPopup(new firebase.auth.EmailAuthProvider())
+    login(email,password) {
+    
+    return this.fireAuth.auth.signInWithEmailAndPassword(email, password)
       .catch(function (error) {
         alert('Please try again');
       });
+    
   }
 }

b5ff5e693b2e9ca92196050f85ca88ce33fea361
AlenaBnkzv <elena.bnkzv@gmail.com>
2019-07-08 20:52:59 +0300

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { AuthGuard } from './shared/guard/auth.guard';
 
 const appRoutes: Routes = [
   { path: '',   redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'profile', component: ProfileComponent },
   { path: 'login', component: LoginPageComponent, canActivate: [AuthGuard] }
 ];
 

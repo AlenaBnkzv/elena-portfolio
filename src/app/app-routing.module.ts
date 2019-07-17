b3d4e0d3fc13b9b964016f41d45369c7a94fb0d5
AlenaBnkzv <elena.bnkzv@gmail.com>
2019-07-17 12:21:43 +0300

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { BioComponent } from './bio/bio.component';
+import { ProjectComponent } from './project/project.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { AuthGuard } from './shared/guard/auth.guard';
 
@@ -9,6 +10,7 @@ const appRoutes: Routes = [
   { path: '',   redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'bio', component: BioComponent },
+   { path: 'projects', component: ProjectComponent },
   { path: 'login', component: LoginPageComponent, canActivate: [AuthGuard] }
 ];
 
